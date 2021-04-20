d056c25893b3f93abb858c455e2f7d8b6ac6a024
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-04-20 16:40:40 +0100

    P tag Hello rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactFlow from 'react-flow-renderer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <p>Hello</p>
     </div>
   );
 }
