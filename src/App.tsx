2065ef87957a846ce3f7ed064ebdf26c38f1d7a0
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-04-21 11:12:02 +0100

    4 attributes set up

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,48 @@ const elements = [
   {
     id: '1',
     type: 'input', // input node
-    data: { label: 'Calories' },
+    data: { label: 
+      <div>
+        Protein
+      <input placeholder="Add weighting"></input> 
+      </div>},
     position: { x: 100, y: 25 },
   },
   // default node
   {
     id: '2',
     // you can also pass a React component as a label
-    data: { label: <div>Default Node</div> },
+    type: 'input',
+    data: { label: 
+      <div>
+        Taste
+      <input placeholder="Add weighting"></input> 
+      </div>},
     position: { x: 300, y: 25 },
   },
   {
     id: '3',
-    type: 'output', // output node
-    data: { label: 'Hannah' },
+    type: 'input', // output node
+    data: { label: 
+      <div>
+        Cost
+      <input placeholder="Add weighting"></input> 
+      </div>},
     position: { x: 500, y: 25 },
   },
   {
     id: '4',
-    type: 'output', // output node
-    data: { label: <div>
-      <input placeholder="Add food here"></input>
-      
+    type: 'input', // output node
+    data: { label: 
+      <div>
+        Calories
+      <input placeholder="Add weighting"></input>
       </div>},
-    position: { x: 500, y: 25 },
+    position: { x: 700, y: 25 },
   },
   // animated lines between 'Nodes'
-  { id: 'e1-2', source: '1', target: '2', animated: true },//animated line
-  { id: 'e2-3', source: '2', target: '3' },//solid line
+  // { id: 'e1-2', source: '1', target: '2', animated: true },//animated line
+  // { id: 'e2-3', source: '2', target: '3' },//solid line
 ];
 
 //style={{height:1000}} is the size of the screen
